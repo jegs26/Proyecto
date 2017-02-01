c230052a91500a7465996c5e965ec0ea9900447a
jegs26 <jegs26@hotmail.com>
2017-01-31 22:14:27 -0500

    Subida 2

---
+++ DAL/TEST/DALSQLServer.TEST/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DALSQLServer.TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
